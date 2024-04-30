84613f15188429309eaaa1dbeda173d71611478c
munaciella <franes88@gmail.com>
2024-04-30 14:06:45 +0100

    tidy files, add img, add index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import type { NextPage } from "next/types";
+//import { copy } from "@/copy";
 
-export default function Home() {
-  return (
+const Homepage: NextPage = () => (
+  <main className="flex flex-col items-center justify-center min-h-screen py-2">
     <h1 className="bg-red-500">Hello</h1>
+    </main>
   )
-}
+
+  export default Homepage;
