cc4741ecb48eccbb8f7cb7fff24a55766055559b
munaciella <franes88@gmail.com>
2024-04-30 23:25:43 +0100

    add index for app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next/types";
 //import { copy } from "@/copy";
 
 const Homepage: NextPage = () => (
-  <main className="flex flex-col items-center justify-center min-h-screen py-2">
+  <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
     <h1 className="bg-red-500">Hello</h1>
     </main>
   )
