78ee29c0252a5ecae5b9d353368e2adac6331f90
munaciella <franes88@gmail.com>
2024-05-01 13:04:12 +0100

    add copy and components folders and files

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NextPage } from "next/types";
+
+const Aboutpage: NextPage = () => {
+  return <h1 className="bg-red-500">About</h1>;
+};
+
+export default Aboutpage;
---
+++ src/copy/index.ts
@@ -0,0 +1,39 @@
+export const copy = {
+    common: {
+      nav: {
+        logo: {
+          alt: "velovegans logo",
+          src: "/assets/small-original.jpg",
+        },
+        home: "Home",
+        about: "About",
+        recipes: "Recipes",
+        contact: "Contact",
+      },
+      footer: {
+        companyName: "VeloVegans.",
+        msg: "All rights reserved.",
+        socials: [
+          { href: "https://www.facebook.com", name: "Facebook" },
+          { href: "https://www.instagram.com", name: "Instagram" },
+          { href: "https://www.x.com", name: "X" },
+        ],
+      },
+    },
+    home: {
+      description: {
+        heading: {
+          top: "VeloVegans",
+          bottom: "Cruelty Free Recipes",
+        },
+        paragraph:
+          "Saving the animals and our planet one meal at a time.",
+        img: {
+          alt: "velovegans logo",
+          src: "/assets/original-logo.jpg",
+        },
+      },
+    
+    },
+  } as const;
+  
