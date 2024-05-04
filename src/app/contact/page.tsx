62d39fef5bb8ae09f962e013087a3245672f2d41
munaciella <franes88@gmail.com>
2024-05-04 23:56:26 +0100

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NextPage } from "next/types";
+
+const Contactpage: NextPage = () => {
+  return <h1 className="bg-red-500">About</h1>;
+};
+
+export default Contactpage;
