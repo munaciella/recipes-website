e24a9f61681efe46b7e7ec62184f5a9380ac8f14
munaciella <franes88@gmail.com>
2024-05-07 23:14:23 +0100

    fix an error with copy file

--- src/copy/index.ts
+++ src/copy/index.ts
@@ -1,39 +1,36 @@
 export const copy = {
-    common: {
-      nav: {
-        logo: {
-          alt: "velovegans logo",
-          src: "/assets/logo_size-removebg-preview.png",
-        },
-        home: "Home",
-        about: "About",
-        recipes: "Recipes",
-        contact: "Contact",
+  common: {
+    nav: {
+      logo: {
+        alt: 'velovegans logo',
+        src: '/assets/logo_size-removebg-preview.png',
       },
-      footer: {
-        companyName: "VeloVegans.",
-        msg: "All rights reserved.",
-        socials: [
-            { href: "https://www.instagram.com", name: "Instagram" },
-          { href: "https://www.threads.com", name: "Threads" },
-          { href: "https://www.x.com", name: "X" },
-        ],
+      home: 'Home',
+      about: 'About',
+      recipes: 'Recipes',
+      contact: 'Contact',
+    },
+    footer: {
+      companyName: 'VeloVegans.',
+      msg: 'All rights reserved.',
+      socials: [
+        { href: 'https://www.instagram.com', name: 'Instagram' },
+        { href: 'https://www.threads.com', name: 'Threads' },
+        { href: 'https://www.x.com', name: 'X' },
+      ],
+    },
+  },
+  home: {
+    description: {
+      heading: {
+        top: 'VeloVegans',
+        bottom: 'Cruelty Free Recipes',
+      },
+      paragraph: 'Saving the animals and our planet one meal at a time.',
+      img: {
+        alt: 'velovegans logo',
+        src: '/assets/original-logo.jpg',
       },
     },
-    home: {
-      description: {
-        heading: {
-          top: "VeloVegans",
-          bottom: "Cruelty Free Recipes",
-        },
-        paragraph:
-          "Saving the animals and our planet one meal at a time.",
-        img: {
-          alt: "velovegans logo",
-          src: "/assets/original-logo.jpg",
-        },
-      },
-    
-    },
-  } as const;
-  
+  },
+} as const;
