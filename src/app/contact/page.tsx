90b3996243932b94d83d531dfa1f12a681848ef3
munaciella <franes88@gmail.com>
2024-05-13 12:20:33 +0100

    fix css on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,45 +4,84 @@ import { copy } from "@/copy";
 
 const { description } = copy.contact;
 
-const Contactpage: NextPage = () => {
+const ContactPage: NextPage = () => {
     const rowsValue: string = '6'
     const rows: number = parseInt(rowsValue)
-  return (
-    <div className="flex flex-col mt-4 mb-20 mx-auto p-6">
-      <div className="flex justify-center items-center">
-        <form 
-        action="https://getform.io/f/lejxRBbj"
-        method="POST"
-        className='flex flex-col w-full md:w-7/12'
-        >
-            <span>{description.heading}</span>
+    return (
+        <div className="flex flex-col items-center mt-10 mb-20 mx-auto p-6 w-full">
+          <form 
+            action="https://getform.io/f/lejxRBbj"
+            method="POST"
+            className='flex flex-col w-full max-w-md text-primary-400 font-semibold rounded-md p-2'>
+            <div className="flex justify-center">
+              <span>{description.heading}</span>
+            </div>
             <input 
-            type="text" 
-            name='name'
-            placeholder='Name'
-            className='p-2 bg-transparent border-2 rounded-md focus:outline-none text-secondary-400'
+              type="text" 
+              name='name'
+              placeholder='Name'
+              className='p-2 bg-transparent border-2 rounded-md focus:outline-none mt-2'
             />
             <input 
-            type="email" 
-            name='email'
-            placeholder='Email'
-            className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none'
+              type="email" 
+              name='email'
+              placeholder='Email'
+              className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none'
             />
             <textarea
-            name="message" 
-            placeholder="Message" 
-            rows={rows}
-            className='p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none'
+              name="message" 
+              placeholder="Message" 
+              rows={rows}
+              className='p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none'
             />
-            <button 
-            type='submit' 
-            className='cursor-pointer text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-secondary-300 to-secondary-600 drop-shadow-md hover:from-primary-300 hover:to-primary-600'>
+            <div className="flex justify-center">
+              <button 
+                type='submit' 
+                className='cursor-pointer inline-block px-8 py-3 text-base font-medium rounded-md text-white bg-gradient-to-r from-secondary-300 to-secondary-600 shadow-md hover:from-primary-300 hover:to-primary-600'>
                 {description.button}
-            </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+              </button>
+            </div>
+          </form>
+        </div>
+      );
+    };
+    
+    export default ContactPage;
+//   return (
+//     <div className="flex flex-col mt-10 mb-20 mx-auto p-6 w-full">
+//       <div className="flex justify-center items-center">
+//         <form 
+//         action="https://getform.io/f/lejxRBbj"
+//         method="POST"
+//         className='flex flex-col w-full text-primary-300 font-semibold rounded-md p-2'>
+//             <span>{description.heading}</span>
+//             <input 
+//             type="text" 
+//             name='name'
+//             placeholder='Name'
+//             className='p-2 bg-transparent border-2 rounded-md focus:outline-none mt-2'
+//             />
+//             <input 
+//             type="email" 
+//             name='email'
+//             placeholder='Email'
+//             className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none'
+//             />
+//             <textarea
+//             name="message" 
+//             placeholder="Message" 
+//             rows={rows}
+//             className='p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none'
+//             />
+//             <button 
+//             type='submit' 
+//             className='cursor-pointer text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-secondary-300 to-secondary-600 drop-shadow-md hover:from-primary-300 hover:to-primary-600'>
+//                 {description.button}
+//             </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Contactpage;
+// export default Contactpage;
