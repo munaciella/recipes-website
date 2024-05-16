5f4ac3569d80ee010a83a5af2b20062cda2a8e1e
munaciella <franes88@gmail.com>
2024-05-16 23:13:34 +0100

    add ts node and jest

--- __tests__/pages/Homepage.spec.tsx
+++ __tests__/pages/Homepage.spec.tsx
@@ -4,7 +4,7 @@ import { renderPage } from "../utils";
 import { copy } from "@/copy";
 import { useScreenMatcher } from "@/hooks";
 
-//const { description, search } = copy.home;
+const { description } = copy.home;
 //const { nav, footer } = copy.common;
 
 jest.mock("../../src/hooks/useScreenMatcher");
@@ -13,8 +13,8 @@ mockUseScreenMatcher.mockReturnValue({ screenMatches: true });
 
 test("renders headings", () => {
   renderPage(<Homepage />);
-  const thePlaceHeading: HTMLElement = screen.getByRole("heading", {
+  const veloVegansHeading: HTMLElement = screen.getByRole("heading", {
     name: `${description.heading.top} ${description.heading.bottom}`,
   });
-  expect(thePlaceHeading).toBeVisible();
+  expect(veloVegansHeading).toBeVisible();
 });
