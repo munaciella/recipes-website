135b91363519c1cb65b42fbc10a918b4a25bd595
munaciella <franes88@gmail.com>
2024-05-17 12:55:13 +0100

    change path to test page

---
+++ tests/utils/renderPage.tsx
@@ -0,0 +1,22 @@
+// import { ReactNode } from "react";
+// import { render } from "@testing-library/react";
+// import RootLayout from "@/app/layout";
+
+// type Overrides = {};
+
+// export const renderPage = (children: ReactNode, overrides?: Overrides) =>
+//   render(<RootLayout>{children}</RootLayout>);
+
+import React, { ReactNode } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import RootLayout from "@/app/layout";
+
+type Overrides = {};
+
+export const renderPage = (
+  children: ReactNode,
+  overrides?: Overrides,
+  options?: Omit<RenderOptions, "queries">
+) => {
+  return render(<RootLayout>{children}</RootLayout>, options);
+};
