0d82bed50275a0a966cd05802f8993379ca1fbb0
munaciella <franes88@gmail.com>
2024-06-16 23:40:03 +0100

    add a link to about page

--- src/copy/index.ts
+++ src/copy/index.ts
@@ -46,11 +46,11 @@ export const copy = {
         "Welcome to our vegan recipes website, where we celebrate compassion, health, and sustainability. We believe that every meal is an opportunity to make a positive impact on the world. Our recipes are not only delicious but also cruelty-free, meaning they contain no animal products. We are passionate about promoting a lifestyle that respects all living beings. As animal lovers, we understand the importance of kindness and empathy towards our fellow creatures. That is why our recipes are created with love and consideration for animals everywhere. We are also deeply committed to environmental sustainability. Our ingredients are sourced responsibly, and we strive to minimize our carbon footprint in every aspect of our operations. Thank you for joining us on this journey towards a healthier, more compassionate world. Together, we can make a difference, one delicious vegan meal at a time. To kickstart your journey towards saving animals and the planet, here are some useful links:",
       usefulLinks: [
         {
-          name: "Instagram",
-          href: "https://www.instagram.com",
+          name: "Cowspiracy",
+          href: "https://www.cowspiracy.com/",
           img: {
-            src: "/assets/instagram.png",
-            alt: "Instagram",
+            src: "/assets/image.png",
+            alt: "Cowspiracy",
           }
         },
         {
