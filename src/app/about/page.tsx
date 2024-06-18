64649f4508dc11578b0e12a04ec5d1502103d7c3
munaciella <franes88@gmail.com>
2024-06-18 23:39:48 +0100

    fix a ag in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,16 +17,16 @@ const AboutPage: NextPage = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-center mt-14">
         {description.usefulLinks.map(({ name, href, img }, idx) => (
           <div key={idx} className="w-full">
-            <div className="w-full rounded-lg overflow-hidden shadow-lg mt-8">
+            <a href={href} className="block w-full rounded-lg overflow-hidden shadow-lg mt-8">
               <img
                 src={img.src}
                 alt={img.alt}
-                className="w-full h-full object-cover"
+                className="w-full h-72 object-cover"
               />
-            </div>
-            <a href={href} className="text-secondary-700 hover:underline">
-            <span className="block text-primary-500 mt-4 mb-2">{name}</span>
             </a>
+            <div className="text-secondary-700 font-semibold">
+              <span className="block text-primary-500 mt-4 mb-2">{name}</span>
+            </div>
           </div>
         ))}
       </div>
