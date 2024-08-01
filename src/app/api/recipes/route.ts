ddebe5ca50a346bd0d7ae762398ce849f135677e
munaciella <franes88@gmail.com>
2024-08-02 00:07:36 +0100

    modify api recipes route

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -5,10 +5,11 @@ import { NextRequest } from 'next/server';
 export async function GET() {
   try {
     const client = await clientPromise;
-    const db = client.db('velovegans'); // Replace with your database name
+    const db = client.db('velovegans');
     const recipes = await db.collection('recipes').find({}).toArray();
     return NextResponse.json(recipes);
   } catch (error) {
+    console.error('Error fetching recipes:', error);
     return NextResponse.json({ message: 'Error fetching recipes' }, { status: 500 });
   }
 }
@@ -16,11 +17,12 @@ export async function GET() {
 export async function POST(request: NextRequest) {
   try {
     const client = await clientPromise;
-    const db = client.db('velovegans'); // Replace with your database name
+    const db = client.db('velovegans');
     const newRecipe = await request.json();
     await db.collection('recipes').insertOne(newRecipe);
     return NextResponse.json(newRecipe, { status: 201 });
   } catch (error) {
+    console.error('Error adding recipe:', error);
     return NextResponse.json({ message: 'Error adding recipe' }, { status: 500 });
   }
 }
--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,16 +1,18 @@
 import { MongoClient } from 'mongodb';
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
+if (process.env.NODE_ENV !== 'production') {
+  console.log('MONGODB_URI:', process.env.MONGODB_URI);
+}
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
 const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
-
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
-}
 
 if (process.env.NODE_ENV === 'development') {
   const globalWithMongoClient = global as typeof globalThis & {
