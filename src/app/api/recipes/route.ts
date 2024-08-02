66fdf64060e1ab22587be9b9bc066f32e5402db0
munaciella <franes88@gmail.com>
2024-08-02 11:26:31 +0100

    fix mongodb connection

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,6 +1,5 @@
+import clientPromise from '@/app/lib/mongodb';
 import { NextResponse } from 'next/server';
-import clientPromise from '../../lib/mongodb';
-import { NextRequest } from 'next/server';
 
 export async function GET() {
   try {
@@ -16,23 +15,6 @@ export async function GET() {
       return NextResponse.json({ message: 'Error fetching recipes', error: error.message }, { status: 500 });
     } else {
       return NextResponse.json({ message: 'Error fetching recipes', error: 'Unknown error' }, { status: 500 });
-    }
-  }
-}
-
-export async function POST(request: NextRequest) {
-  try {
-    const client = await clientPromise;
-    const db = client.db('velovegans');
-    const newRecipe = await request.json();
-    await db.collection('recipes').insertOne(newRecipe);
-    return NextResponse.json(newRecipe, { status: 201 });
-  } catch (error) {
-    console.error('Error adding recipe:', error);
-    if (error instanceof Error) {
-      return NextResponse.json({ message: 'Error adding recipe', error: error.message }, { status: 500 });
-    } else {
-      return NextResponse.json({ message: 'Error adding recipe', error: 'Unknown error' }, { status: 500 });
     }
   }
 }
--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -1,5 +1,9 @@
 import { MongoClient } from 'mongodb';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('MONGODB_URI:', process.env.MONGODB_URI);
+}
+
 const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error('Please add your Mongo URI to .env.local');
@@ -14,7 +18,6 @@ if (process.env.NODE_ENV === 'development') {
   const globalWithMongoClient = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
-  
   if (!globalWithMongoClient._mongoClientPromise) {
     client = new MongoClient(uri, options);
     globalWithMongoClient._mongoClientPromise = client.connect();
@@ -22,12 +25,16 @@ if (process.env.NODE_ENV === 'development') {
   } else {
     console.log('Reusing existing MongoDB connection in development mode...');
   }
-  
+
   clientPromise = globalWithMongoClient._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
   console.log('Connecting to MongoDB in production mode...');
 }
+
+clientPromise
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(error => console.error('MongoDB connection error:', error));
 
 export default clientPromise;
