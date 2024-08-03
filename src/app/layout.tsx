3389a290fe61f5d0ec98b64effafe1511618e1c7
munaciella <franes88@gmail.com>
2024-08-04 00:05:36 +0100

    add dark theme and theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from "next";
-import { ReactNode } from "react";
-import { Footer, Header } from "@/components";
-import { copy } from "@/copy";
-import "./globals.css";
-
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
+import { Footer, Header } from '@/components';
+import { copy } from '@/copy';
+import './globals.css';
 //import "@/styles/globals.css"
-import { Inter as FontSans } from "next/font/google"
-
-import { cn } from "@/lib/utils"
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
-  title: "VeloVegans",
-  description: "Cruelty free recipes",
+  title: 'VeloVegans',
+  description: 'Cruelty free recipes',
 };
 
 const { footer } = copy.common;
@@ -27,20 +26,29 @@ const RootLayout = ({
   children: ReactNode;
 }>) => (
   <html lang="en" suppressHydrationWarning>
-    <body className={cn(
-          "h-screen w-screen min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>
-      <link rel="icon" href="/assets/favicon.ico" />
-      <div className="h-full max-w-9xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto w-full h-full max-w-8xl flex flex-col items-center justify-between px-2 sm:px-6 lg:px-8">
-          <div className="w-full mx-auto flex flex-col items-center justify-between">
-            <Header />
-            {children}
+    <body
+      className={cn(
+        'h-screen w-screen min-h-screen bg-background font-sans antialiased',
+        fontSans.variable
+      )}
+    >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <link rel="icon" href="/assets/favicon.ico" />
+        <div className="h-full max-w-9xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto w-full h-full max-w-8xl flex flex-col items-center justify-between px-2 sm:px-6 lg:px-8">
+            <div className="w-full mx-auto flex flex-col items-center justify-between">
+              <Header />
+              {children}
+            </div>
+            <Footer companyName={footer.companyName} />
           </div>
-          <Footer companyName={footer.companyName} />
         </div>
-      </div>
+      </ThemeProvider>
     </body>
   </html>
 );
