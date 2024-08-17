ba1e12652dd2b1ca3c1c363f2fb4356092803180
munaciella <franes88@gmail.com>
2024-08-17 15:13:59 +0100

    add supabase implementation to display recipes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hlvrqkpohsfbqqmpruhx.supabase.co',
+        pathname: '/storage/v1/object/sign/recipes-images/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
