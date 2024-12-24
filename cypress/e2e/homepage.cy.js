ce263825b0f7a68c12e5c65bc4f07612c98b44c7
munaciella <franes88@gmail.com>
2024-12-24 10:54:05 +0100

    add test file for homepage in cypress

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,15 +1,38 @@
 describe('Homepage', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-  
-    it('should load the homepage', () => {
-      cy.get('h1').should('contain', 'VeloVegans');
-    });
-  
-    it('should have a functioning navigation menu', () => {
-      cy.get('nav').should('exist');
-      cy.get('nav a').contains('About').click();
-      cy.url().should('include', '/about');
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should load the homepage with the correct heading', () => {
+    cy.get('h1').should('contain', 'VeloVegans');
+    cy.get('h1').should('contain', 'Cruelty Free Recipes');
+  });
+
+  it('should display the navigation menu and allow navigation to the About page', () => {
+    cy.get('nav').should('exist');
+    cy.get('nav a').contains('About').click();
+    cy.url().should('include', '/about');
+  });
+
+  it('should display the Get Started and Browse Recipes buttons', () => {
+    cy.contains('button', 'Get Started').should('exist').click();
+    cy.url().should('include', '/login');
+
+    cy.visit('/');
+    cy.contains('button', 'Browse Recipes').should('exist').click();
+    cy.url().should('include', '/recipes');
+  });
+
+  it('should display the news section with articles', () => {
+    cy.get('section h2').should('contain', 'Check Out the Latest News');
+    cy.get('section').within(() => {
+      cy.wait(1000);
+      cy.get('a').should('have.length.at.least', 1); 
     });
   });
+
+  it('should display the "Show More News" button and load more news', () => {
+    cy.contains('button', 'Show More News').should('exist').click();
+    cy.get('a').should('have.length.at.least', 1);
+  });
+});
